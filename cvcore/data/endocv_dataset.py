49888c32c7b37141a14409b6af42e0f8cbfe7fe9
moewiee <ntd.2404@gmail.com>
2020-02-20 14:49:47 +0700

    remove some augs

--- cvcore/data/endocv_dataset.py
+++ cvcore/data/endocv_dataset.py
@@ -22,33 +22,34 @@ class EDDDataset(Dataset):
         self.mask1_dir = cfg.DIRS.MASK1
         self.img_size = (cfg.DATA.IMG_SIZE, cfg.DATA.IMG_SIZE)
         if mode != "test":
-            self.df = pd.read_csv(f"./data/{mode}_fold{cfg.DATA.FOLD}.csv")
+            self.df = pd.read_csv(f"./data/slices_mask/{mode}_fold{cfg.DATA.FOLD}.csv")
         if mode == "train":
             self.aug = A.Compose([
-                A.OneOf([
-                    A.HorizontalFlip(p=1.),
-                    A.VerticalFlip(p=1.),
-                ]),
-                A.ShiftScaleRotate(
-                        shift_limit=0.0625,
-                        scale_limit=0.1,
-                        rotate_limit=30,
-                        border_mode=cv2.BORDER_CONSTANT,
-                        value=0,
-                        p=1.),
+                # A.OneOf([
+                #     A.HorizontalFlip(p=1.),
+                #     A.VerticalFlip(p=1.),
+                # ]),
+                A.HorizontalFlip(p=0.5),
+                # A.ShiftScaleRotate(
+                #         shift_limit=0.0625,
+                #         scale_limit=0.1,
+                #         rotate_limit=30,
+                #         border_mode=cv2.BORDER_CONSTANT,
+                #         value=0,
+                #         p=1.),
                 A.GridDistortion(
                     distort_limit=0.2,
                     border_mode=cv2.BORDER_CONSTANT,
                     value=0,
                     p=1.),
                 A.OneOf([
-                    A.IAAAdditiveGaussianNoise(p=1.),
-                    A.GaussNoise(p=1.),
-                ]),
-                A.OneOf([
-                    A.MedianBlur(blur_limit=3, p=1.),
-                    A.Blur(blur_limit=3, p=1.),
-                ]),
+                    A.IAAAdditiveGaussianNoise(),
+                    A.GaussNoise(),
+                ], p=0.5),
+                # A.OneOf([
+                #     A.MedianBlur(blur_limit=3, p=1.),
+                #     A.Blur(blur_limit=3, p=1.),
+                # ]),
                 A.Normalize(),
                 A.pytorch.ToTensorV2()
             ])
@@ -67,34 +68,10 @@ class EDDDataset(Dataset):
         img = Image.open(os.path.join(self.data_dir, img_id + ".jpg")).convert("RGB")
         img = np.asarray(img, dtype=np.uint8)
 
-
-        # img_label = self.df[self.classes].values[idx]
-        # if img_id.startswith('Abnormal') or img_id.startswith('ETIS-LaribPolypDB'):
-        #     img = Image.open(os.path.join(
-        #         self.data_dir, img_id))
-        # else:
-        #     img = Image.open(os.path.join(
-        #         self.data_dir, "originalImages", img_id))
-        # img = np.asarray(img, dtype=np.uint8)
-
         if self.mode != "test":
             mask = np.zeros((*img.shape[:-1], 2))
-            mask[..., 0] = np.asarray(Image.open(os.path.join(self.mask0_dir, img_id.replace("volume", "segmentation") + ".png")).convert('L'), dtype=np.uint8)
-            mask[..., 1] = np.asarray(Image.open(os.path.join(self.mask1_dir, img_id.replace("volume", "segmentation") + ".png")).convert('L'), dtype=np.uint8)
-            # for i, cl in enumerate(self.classes):
-            #     if img_label[i] == 1:
-            #         if img_id.startswith('Abnormal'):
-            #             mask_id = img_id.replace('Abnormal', 'Abnormal_Annotation') \
-            #                 .replace('.jpg', '_Annotation.bmp')
-            #         elif img_id.startswith('ETIS-LaribPolypDB'):
-            #             mask_id = img_id.replace('ETIS-LaribPolypDB/',
-            #                 'ETIS-LaribPolypDB-Masks/p')
-            #         else:
-            #             mask_id = "masks/" + img_id.replace(".jpg", f"_{cl}.tif")
-
-            #         mask[..., i] = np.asarray(Image.open(
-            #             os.path.join(self.data_dir, mask_id)).convert('L'),
-            #             dtype=np.uint8)
+            mask[..., 0] = np.asarray(Image.open(os.path.join(self.mask0_dir, img_id + ".jpg")).convert('L'), dtype=np.uint8)
+            mask[..., 1] = np.asarray(Image.open(os.path.join(self.mask1_dir, img_id + ".jpg")).convert('L'), dtype=np.uint8)
             mask = np.where(mask > 0, 255, 0)
             img = cv2.resize(img, self.img_size,
                 interpolation=cv2.INTER_LINEAR)
